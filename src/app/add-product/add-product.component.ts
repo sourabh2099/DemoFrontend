43666d1cfc8d263bcf02b07e116de8090dd4ff39
sourabh2099 <sourabhchowdhury82@gmail.com>
2021-09-18 20:38:24 +0530

    sourabh 2

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductServiceService } from '../product-service.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  product:Product = new Product();
 
-  constructor() { }
+  constructor(private productService:ProductServiceService) { }
 
   ngOnInit() {
+  }
+
+  add():void{
+    console.log("inside add fucntion");
+      console.log(this.product);
+      this.productService.addProduct(this.product).subscribe(
+        data=>{console.log(data);
+        
+        }
+      )
+      ;
   }
 
 }
